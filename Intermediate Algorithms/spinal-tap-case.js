27a098965a4b3f1a59df8217bb05ab2990e0adc4
Gerard Hynes <gkhynes@gmail.com>
2019-04-14 16:47:48 +0100

    Add explanation to spinal-tap-case

--- Intermediate Algorithms/spinal-tap-case.js
+++ Intermediate Algorithms/spinal-tap-case.js
@@ -1,3 +1,5 @@
+// Convert a string to spinal case, removing any spaces or underscores
+
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
@@ -10,3 +12,9 @@ function spinalCase(str) {
 }
 
 spinalCase("This Is Spinal Tap");
+
+/*
+1. Create a regex for whitespace and underscores
+2. Use .replace() and another regex to replace adjacent lowercase and uppercase characters with lowercase + space + uppercase characters.
+3. Convert the str to lowercase and use the first regex to replace spaces and underscores with dashes.
+*/
