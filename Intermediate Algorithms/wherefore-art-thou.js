9a05dc47c1e5b0a36c58b3a46b4e0320d87db13b
Gerard Hynes <gkhynes@gmail.com>
2019-04-14 12:41:49 +0100

    Complete wherefore-art-thou.js

---
+++ Intermediate Algorithms/wherefore-art-thou.js
@@ -0,0 +1,18 @@
+function whatIsInAName(collection, source) {
+  const sourceKeys = Object.keys(source);
+
+  return collection.filter(item => {
+    return sourceKeys.every(key => {
+      return item.hasOwnProperty(key) && item[key] === source[key];
+    });
+  });
+}
+
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }
+  ],
+  { last: "Capulet" }
+);
