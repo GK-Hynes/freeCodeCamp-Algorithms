ff4b1846367558b9a2726f74d248924eb4fda816
Gerard Hynes <gkhynes@gmail.com>
2019-05-19 11:22:18 +0100

    Update caesars-cipher.js

--- javascript-algorithms-and-data-structures-projects/caesars-cipher.js
+++ javascript-algorithms-and-data-structures-projects/caesars-cipher.js
@@ -1,16 +1,16 @@
 /*Write a function which takes a ROT13 (Caesar's cipher) encoded string as input and returns a decoded string.*/
 
 function rot13(str) {
-  var newStr = str
+  let newStr = str
     .split("")
     .map(function(char) {
-      x = char.charCodeAt(0);
-      if (x < 65 || x > 90) {
-        return String.fromCharCode(x);
-      } else if (x < 78) {
-        return String.fromCharCode(x + 13);
+      let charCode = char.charCodeAt(0);
+      if (charCode < 65 || charCode > 90) {
+        return String.fromCharCode(charCode);
+      } else if (charCode < 78) {
+        return String.fromCharCode(charCode + 13);
       }
-      return String.fromCharCode(x - 13);
+      return String.fromCharCode(charCode - 13);
     })
     .join("");
 
@@ -25,7 +25,7 @@ rot13("SERR PBQR PNZC");
 2. If a character has an ASCII value below 65 or above 90, return it unchanged.
   This preserves punctuation and spaces. 
 3. If a character's ASCII value is less than 78, add 13 and return it. 
-  Since N is the midpoint of the alphabet, this is the same as subtracting until you get to A and then subtracting from Z. (e.g.You want C, 67, to onvert to P, 80)
+  Since N is the midpoint of the alphabet, this is the same as subtracting until you get to A and then subtracting from Z. (e.g. You want C, 67, to convert to P, 80)
 4. Otherwise return the character's ASCII value minus 13.
 5. Finally use join to convert the array back into a string. 
 */
